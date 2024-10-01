169c32c1568c2c5de657a7933a42dc65975db53a
juliana <jumarquez28@gmail.com>
2024-10-01 20:04:43 -0300

    Resolução questão 5

--- todos/views.py
+++ todos/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 
+from django.http import HttpResponse
+def home(request):
+	return HttpResponse('Hello world');
+
 def home(request):
     return render(request, 'todos/home.html');
